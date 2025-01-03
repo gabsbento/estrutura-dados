869168499b66d77330fcc414fe46018af4d33d24
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-03 16:55:03 -0300

    :sparkles: feat: aula2

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import dados.Vetor;
+
 public class Main {
     public static void main(String[] args){
+        Vetor vetor = new Vetor(10);
 
+        System.out.println(vetor);
     }
 }
---
+++ dados/Vetor.java
@@ -0,0 +1,9 @@
+package dados;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
